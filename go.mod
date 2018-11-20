18c65a3589f2bbaa3faef893b4dddf9971e28c91
Marko Mikulicic <mkm@bitnami.com>
2018-11-21 00:25:10 +0100

    Add status.proto

--- go.mod
+++ go.mod
@@ -3,5 +3,7 @@ module github.com/bitnami-labs/udig
 require (
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/golang/protobuf v1.2.0
+	github.com/googleapis/googleapis v0.0.0-20181120215828-b7a1d68ea384 // indirect
+	google.golang.org/genproto v0.0.0-20180817151627-c66870c02cf8
 	google.golang.org/grpc v1.16.0
 )
--- pkg/uplink/uplinkpb/build.go
+++ pkg/uplink/uplinkpb/build.go
@@ -1,5 +1,4 @@
-//go:generate protoc -I. -I../../.. --go_out=plugins=grpc,paths=source_relative:../../.. pkg/uplink/uplinkpb/uplink.proto
+//go:generate go get -d github.com/googleapis/googleapis@v0.0.0-20181120215828-b7a1d68ea384
+//go:generate bash -c "protoc -I. -I../../.. --go_out=plugins=grpc,paths=source_relative:../../.. -I $GOPATH/pkg/mod/github.com/googleapis/googleapis@v0.0.0-20181120215828-b7a1d68ea384 pkg/uplink/uplinkpb/uplink.proto"
 
 package uplinkpb
-
-//	_ "github.com/googleapis/googleapis/google/rpc"
